bdcba6157141724e7db325b59894ef313aff248d
Stepan14511 <stepan14511@gmail.com>
2018-04-02 23:10:29 +0300

    Added useless button ROLL and lcd for use it in future. Added them to VBox.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLCDNumber, QVBoxLayout
 from PyQt5.QtGui import QIcon
 
 
@@ -11,6 +11,13 @@ class Window(QWidget):
         self.init_ui()
 
     def init_ui(self):
+        button_roll = QPushButton('Roll', self)
+        lcd = QLCDNumber(self)
+        vbox = QVBoxLayout()
+        vbox.addWidget(lcd)
+        vbox.addWidget(button_roll)
+        self.setLayout(vbox)
+
         self.setGeometry(300, 300, 300, 220)
         self.setWindowTitle('Dice Rolling Simulator')
         self.setWindowIcon(QIcon('img/dice.png'))
